de016f86efae85762fb0aeab8861807382fcbf74
lukevoss <luke@familievoss.net>
2020-11-19 18:39:14 +0100

    deleted comments

--- Practica7/minimaxAlum.c
+++ Practica7/minimaxAlum.c
@@ -17,8 +17,6 @@ tNodo* minimax(tNodo *t, int jugador) {
      int mejorJugada = -1;
      int max_actual, jugada;
      int max = -10000;
-     //int puntos = -2;
-     //int i, temp;
      tNodo *intento=malloc(sizeof(tNodo));
      printf("\n Mi turno: \n");
      for(jugada = 0; jugada < 9; ++jugada){
@@ -50,56 +48,6 @@ tNodo *jugadaAdversario(tNodo *t) {
      return t;
 }
 
-//int minimax(int board[9], int player) {
-//    //How is the position like for player (their turn) on board?
-//    int winner = win(board);
-//    if (winner != 0) return winner * player;
-//
-//    move = -1;
-//    int score = -2;//Losing moves are preferred to no move
-//    int i;
-//    for (i = 0; i < 9; ++i) {//For all moves,
-//        if (board[i] == 0) {//If legal,
-//            board[i] = player;//Try the move
-//            int thisScore = -minimax(board, player * -1);
-//            if (thisScore > score) {
-//                score = thisScore;
-//                //move = i;
-//            }//Pick the one that's worst for the opponent
-//            board[i] = 0;//Reset board after try
-//        }
-//    }
-//    if (move == -1) return 0;
-//    return score;
-//}
-//
-//void computerMove(int board[9]) {
-//    int move = -1;
-//    int score = -2;
-//    int i;
-//    for (i = 0; i < 9; ++i) {
-//        if (board[i] == 0) {
-//            board[i] = 1;
-//            int tempScore = -minimax(board, -1);
-//            board[i] = 0;
-//            if (tempScore > score) {
-//                score = tempScore;
-//                move = i;
-//            }
-//        }
-//    }
-//    //returns a score based on minimax tree at a given node.
-//    board[move] = 1;
-//}
-//
-//void playerMove(int board[9]) {
-//    int move = 0;
-//    do {
-//        printf("\nInput move ([0..8]): ");
-//        scanf("%d", &move);
-//        printf("\n");
-//    } while (move >= 9 || move < 0 && board[move] == 0);
-//    board[move] = -1;
-//}
+
 
 
