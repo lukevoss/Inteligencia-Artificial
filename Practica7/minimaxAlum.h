481c69e8e016334dc32677a822e4b7b032cf7c52
Henri <henri.smidt@t-online.de>
2020-11-26 11:23:32 +0100

    bal

--- Practica7/minimaxAlum.h
+++ Practica7/minimaxAlum.h
@@ -8,3 +8,9 @@
 
 tNodo *jugadaAdversario(tNodo *t);
 tNodo* minimax(tNodo* t, int jugador, int limite);
+int valorMin(tNodo* t, int profundidad);
+int valorMax(tNodo* t, int profundidad);
+tNodo* jugadaAdversario(tNodo* t);
+int valorMin_ab(tNodo* t, int prof, int alfa, int beta);
+int valorMax_ab(tNodo* t, int prof, int alfa, int beta);
+tNodo* poda_ab(tNodo* t, int jugador);
